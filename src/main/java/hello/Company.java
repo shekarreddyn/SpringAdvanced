d69d435e897ccbedf2652d5fa2de7e5ffc11e7d7
Shekar <shekaredu16@gmail.com>
2018-03-25 23:12:27 -0500

    Spring boot annotations

---
+++ src/main/java/hello/Company.java
@@ -0,0 +1,37 @@
+package hello;
+
+import java.util.*;
+
+public class Company {
+
+    private String companyName;
+    private int noOfEmployees;
+    private List<Employee> employeeList;
+
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getNoOfEmployees() {
+        return noOfEmployees;
+    }
+
+    public void setNoOfEmployees(int noOfEmployees) {
+        this.noOfEmployees = noOfEmployees;
+    }
+
+    public List<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(List<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+
+}
---
+++ src/main/java/hello/Employee.java
@@ -0,0 +1,42 @@
+package hello;
+import java.util.*;
+
+public class Employee {
+
+    private String employeeName;
+    private int employeeId;
+    private int age;
+    private List<Address> addresses;
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
